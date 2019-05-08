2268fdc3e0c260ac996d0e898a06eaff64267d3e
LUCAS NATALICIO FERREIRA <1090481811023@fatec.local>
2019-05-08 12:21:21 -0300

    loginAdd

--- distribuicaoBin.js
+++ distribuicaoBin.js
@@ -1,11 +1,52 @@
 function entrada(){
-    var n = Number(document.getElementById("valorN").value);
-    var k = Number(document.getElementById("valorK").value);
-    var p = Number(document.getElementById("valorP").value);
-    var q = Number(document.getElementById("valorQ").value);
-    var dp = 0;
-    var media = n*p;
-    dp = Math.sqrt(n*p*q).toFixed(2);
-    document.getElementById("saida1").innerHTML = "A media é: " + media +"</br>O desvio padrão é: " + dp;
+    var n = Number(document.querySelector("input#valorN").value);
+    var k = document.querySelector("input#valorK").value;
+    var p = Number(document.querySelector("input#valorP").value);
+    var q = Number(document.querySelector("input#valorQ").value);
 
+    var mediaF = mediaA(n,p);
+    var desvioP = deviopadrao(n,p,q);
+
+    k = k.split(";");
+    var prob = null;
+    k.forEach(function(element,i) {
+        prob += probabilidade(p,Number(element),n,q);
+    }, this);
+
+    document.getElementById("saida1").innerHTML = "A media é: " + mediaF +"</br>O desvio padrão é: " + desvioP + "</br>  " + " A probabilidade é de: " + (prob * 100).toFixed(2) +"%";
+
+}
+function mediaA(n,p){
+    var media = n*p;
+    return media;
+}
+
+function deviopadrao(n,p,q){
+    dp = Math.sqrt(n*p*q).toFixed(2);
+    return dp;
+}
+function probabilidade(p,k,n,q){
+    var analiseresult = analise(n,k);
+    var resultado = analiseresult * Math.pow(p,k) * Math.pow(q,(n -k));
+    return resultado
+}
+function analise(n,k){
+    if(k ==0 || n == k){
+        return 1;
+    }
+    else if(k==1 ){
+        return n;
+    }
+    var fatn = fatorial(n);
+    var fatk = fatorial(k);
+    var fatnk = fatorial(n - k);
+    var analise = fatn / (fatk * fatnk);
+    return analise;
+}
+function fatorial(n){
+    var result = n;
+    for(var i = 1; i <n; i++){
+        result = result * i;
+    }
+    return result;          
 }
