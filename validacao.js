10cae3c3a15ecf8fcf39f3474fef4f1cf2f928f2
Lucas Natalicio Ferreira <lucasnf99@gmail.com>
2019-05-11 21:25:55 -0300

    validaçaoLogin

--- validacao.js
+++ validacao.js
@@ -1,4 +1,7 @@
 function validacao(){
-    
+    if((document.form.email.value == "")||(document.form.senha.value == "")){
+        alert("Por favor preecha todos os campos!");
+        return false;
+    }
 }
 
